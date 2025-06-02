8446665844fdb1c1c67f83d08c02e24e3d348aa9
trieuvn <trieu123ok@gmail.com>
2025-06-02 17:19:38 +0700

    add model properties

--- src/main/java/com/uef/model/LOCATION.java
+++ src/main/java/com/uef/model/LOCATION.java
@@ -8,8 +8,8 @@ package com.uef.model;
  *
  * @author sang
  */
-
 public class LOCATION {
+
     private int id;
     private float latitude;
     private float longitude;
@@ -17,7 +17,9 @@ public class LOCATION {
     private String placeAt;
 
     // Constructors
-    public LOCATION() {}
+    public LOCATION() {
+    }
+
     public LOCATION(int id, float latitude, float longitude, String name, String placeAt) {
         this.id = id;
         this.latitude = latitude;
@@ -27,14 +29,43 @@ public class LOCATION {
     }
 
     // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public float getLatitude() { return latitude; }
-    public void setLatitude(float latitude) { this.latitude = latitude; }
-    public float getLongitude() { return longitude; }
-    public void setLongitude(float longitude) { this.longitude = longitude; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getPlaceAt() { return placeAt; }
-    public void setPlaceAt(String placeAt) { this.placeAt = placeAt; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlaceAt() {
+        return placeAt;
+    }
+
+    public void setPlaceAt(String placeAt) {
+        this.placeAt = placeAt;
+    }
 }
