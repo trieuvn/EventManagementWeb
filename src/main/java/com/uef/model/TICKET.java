677fddfe33e0bb1dd599a059100da3a56d941842
trieuvn <trieu123ok@gmail.com>
2025-06-26 22:41:40 +0700

    fixed issue

--- src/main/java/com/uef/model/TICKET.java
+++ src/main/java/com/uef/model/TICKET.java
@@ -267,36 +267,7 @@ public class TICKET {
         }
         return this.slots - this.participants.size();
     }
-    public List<Integer> getRates() {
-        List<Integer> rates = new ArrayList<>();
-        if (this.participants != null) {
-            for (PARTICIPANT participant : this.participants) {
-                if (participant.getRate() != 0) { // Giả sử 0 là giá trị mặc định hoặc không hợp lệ
-                    rates.add(participant.getRate());
-                }
-            }
-        }
-        return rates;
-    }
-
-    public float getAvgRate() {
-        List<Integer> rates = getRates();
-        if (rates == null || rates.isEmpty()) {
-            return 0.0f;
-        }
-        int sum = 0;
-        for (Integer rate : rates) {
-            sum += rate;
-        }
-        return (float) sum / rates.size();
-    }
-
-    public int getAvailableSlots() {
-        if (this.participants == null) {
-            return this.slots;
-        }
-        return this.slots - this.participants.size();
-    }
+    
 
     public String createQRCode() throws UnsupportedEncodingException, IOException {
     // Tạo chuỗi kết hợp ticket id với thông tin khác (ví dụ: id và confirmCode)
